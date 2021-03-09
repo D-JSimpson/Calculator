ddeb572d8baa970e13f01aae5d66fc38cc037c63
dante <simpsondante03@gmail.com>
2021-03-09 14:40:58 -0500

    Make all the functions work

--- script.js
+++ script.js
@@ -29,22 +29,24 @@ function factorial(fac) {
 function operate(operator, a, b){
     switch (operator){
         case '+':
-            console.log(add(a, b));
+            display.innerText  = add(+a, +b);
             break;
         case '-':
-            console.log(subtract(a, b));
+            display.innerText  = subtract(+a, +b);
             break;
         case '*':
-            console.log(multiply(a, b));
+            display.innerText  = multiply(+a, +b);
             break;
         case '/':
-            console.log(divide(a, b));
+            display.innerText  = divide(+a, +b);
             break;
         case '^':
-            console.log(power(a, b));
+            display.innerText  = power(+a, +b);
             break;
         case '!':
-            console.log(factorial(a));
+            if(a !== ''){
+            display.innerText  = factorial(+a);
+            } else { display.innerText  = factorial(+b); }
             break;
     }
 }
@@ -52,18 +54,23 @@ function operate(operator, a, b){
 const clear = document.getElementById('clear');
 const display = document.getElementById('display');
 const buttons = document.querySelectorAll('button');
+const equals = document.getElementById('equals');
 const buttonsArr = Array.from(buttons);
 const numbers = buttonsArr.filter(button => (button.value !== ""));
 console.log(numbers);
 
 let operand1 = '';
 let operand2 = '';
-clear.addEventListener('click', function(){display.innerText = ""; operand1 = ''; operand2 = '';});
+let operator = '';
+clear.addEventListener('click', function(){display.innerText = ""; operand1 = ''; operand2 = ''; operator = '';});
+
+equals.addEventListener('click', function(){operate(operator, operand1, operand2)})
 
 numbers.forEach(button => button.addEventListener('click', function(){
     let end = (display.innerText.substring(display.innerText.length - 1,display.innerText.length));
     if(this.value % 1 !== 0 && this.value !== '!' && this.value !== '^'){
         display.innerText += (" " + this.value);
+        operator = '' + this.value + '';
         }
     else if(end % 1 !== 0 && end !== '^'){
         if(this.value == '^'){
@@ -74,11 +81,21 @@ numbers.forEach(button => button.addEventListener('click', function(){
     }
     else{
         display.innerText += this.value;
+        if(this.value % 1 === 0){
         operand2 += this.value;
+        }
+        if(this.value == '!'){
+            operator = '' + this.value + '';
+            }
+        if(this.value == '^'){
+                operator = '' + this.value + '';
+                operand1 = operand2;
+                operand2 = '';
+                }
         console.log(operand2);
         console.log(operand1);
-        console.log(typeof operand2);
+        console.log(operator);
     }
 }));
-//fix pow;
+
 

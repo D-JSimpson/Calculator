785607483ca7f18de66d697fc8e1da67bf1ffba0
dante <simpsondante03@gmail.com>
2021-03-09 13:57:33 -0500

    Create variables that store the operands

--- script.js
+++ script.js
@@ -51,19 +51,33 @@ function operate(operator, a, b){
 
 const clear = document.getElementById('clear');
 const display = document.getElementById('display');
-clear.addEventListener('click', function(){display.innerText = "";});
 const buttons = document.querySelectorAll('button');
 const buttonsArr = Array.from(buttons);
 const numbers = buttonsArr.filter(button => (button.value !== ""));
 console.log(numbers);
+
+let operand1 = '';
+let operand2 = '';
+clear.addEventListener('click', function(){display.innerText = ""; operand1 = ''; operand2 = '';});
+
 numbers.forEach(button => button.addEventListener('click', function(){
     let end = (display.innerText.substring(display.innerText.length - 1,display.innerText.length));
     if(this.value % 1 !== 0 && this.value !== '!' && this.value !== '^'){
         display.innerText += (" " + this.value);
         }
-    else if(end % 1 !== 0 && end !== '^'){if(this.value == '^'){display.innerText += this.value;} else {display.innerText += (" " + this.value);}}
+    else if(end % 1 !== 0 && end !== '^'){
+        if(this.value == '^'){
+            display.innerText += this.value;
+        } else {display.innerText += (" " + this.value);}
+        operand1 = operand2;
+        operand2 = '' + this.value + '';
+    }
     else{
         display.innerText += this.value;
+        operand2 += this.value;
+        console.log(operand2);
+        console.log(operand1);
+        console.log(typeof operand2);
     }
 }));
 //fix pow;
